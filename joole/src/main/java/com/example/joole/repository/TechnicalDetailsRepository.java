7423afa56d23afde516a076ed4954d1611e6fbbe
Karim Ahmed <31129368+KarimIAhmed@users.noreply.github.com>
2022-10-07 10:55:32 -0400

    Delete joole directory

--- joole/src/main/java/com/example/joole/repository/TechnicalDetailsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.joole.repository;
-
-import com.example.joole.model.TechnicalDetails;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TechnicalDetailsRepository extends JpaRepository<TechnicalDetails,Long> {
-
-}
